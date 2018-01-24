0884d36af0d41d5d8e47cbecab999910ab1bf801
Ara <ayaghsizian@gmail.com>
2018-01-23 19:37:44 -0500

    created class Node

--- PlinkoAsBinaryTree/Program.cs
+++ PlinkoAsBinaryTree/Program.cs
@@ -6,6 +6,28 @@ using System.Threading.Tasks;
 
 namespace PlinkoAsBinaryTree
 {
+
+    class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int initial)
+        {
+            value = initial;
+            left = null;
+            right = null;
+        }
+    }// class Node
+
+    class Tree
+    {
+        Node top;
+
+
+    }// class Tree
+
     class Program
     {
         static void Main(string[] args)
